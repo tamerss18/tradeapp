21ff3427b919914f807b07bb7df712081f4aafce
tamer safadi <xdox.x2@gmail.com>
2018-03-18 12:10:32 +0200

    upgrading the design

--- app/src/main/java/com/example/tamer/trade/LogIn.java
+++ app/src/main/java/com/example/tamer/trade/LogIn.java
@@ -1,8 +1,10 @@
 package com.example.tamer.trade;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.view.View;

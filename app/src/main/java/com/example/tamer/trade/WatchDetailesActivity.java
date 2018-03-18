4e26b7e7a7e05dff745e535a2ed61b82266de6dd
tamer safadi <xdox.x2@gmail.com>
2018-03-18 12:13:51 +0200

    design changes

---
+++ app/src/main/java/com/example/tamer/trade/WatchDetailesActivity.java
@@ -0,0 +1,13 @@
+package com.example.tamer.trade;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WatchDetailesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_watch_detailes);
+    }
+}
